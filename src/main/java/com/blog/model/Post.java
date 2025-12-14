5cc136012a9f609c35c8338d57b104e5845a3a62
Alex Al <Alex@al.com>
2025-12-14 17:45:02 +0300

    добавит описание schema.sql; добавит model;

---
+++ src/main/java/com/blog/model/Post.java
@@ -0,0 +1,57 @@
+package com.blog.model;
+
+public class Post {
+
+    private Long id;
+
+    private String title;
+
+    private String text;
+
+    private Integer likesCount = 0;
+
+    private Integer commentsCount = 0;
+
+    public Post() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(Integer likesCount) {
+        this.likesCount = likesCount;
+    }
+
+    public Integer getCommentsCount() {
+        return commentsCount;
+    }
+
+    public void setCommentsCount(Integer commentsCount) {
+        this.commentsCount = commentsCount;
+    }
+}
